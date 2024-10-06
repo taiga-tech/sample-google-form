8cc0631e9f9d953c07632fd8f315f2dd8dd8877e
taiga-tech <taiga.dev.n@gmail.com>
2024-10-06 19:27:19 +0900

    Update form-builder.tsx

--- src/components/form-builder.tsx
+++ src/components/form-builder.tsx
@@ -3,35 +3,16 @@
 // types
 import type { InputFieldProps } from '@/const/form'
 // react
-import { useEffect, useState } from 'react'
+import { useState, useCallback } from 'react'
 // clsx
 import clsx from 'clsx'
 // react-hool-form
 import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
 //yup
 import * as yup from 'yup'
 import { jpConfig } from '@/lib/yup.locale'
-// lib
-// import isObjectEmpty from '@/lib/is-object-empty'
-// components
-// import SnackBar from '@/components/base/snackbar'
-// icon
-// import InfoIcon from '@/svg/icon/info.svg'
-// import ErrorICon from '@/svg/icon/error.svg'
 
 export type Status = 'initial' | 'loading' | 'failure' | 'sent'
-
-// export interface FormProps {
-//     company: string
-//     name: string
-//     kana: string
-//     tel: string
-//     email: string
-//     category: string
-//     message: string
-//     confirm: boolean
-// }
 
 const isObjectEmpty = (obj: any) => Boolean(Object.keys(obj).length)
 
@@ -39,27 +20,15 @@ interface FormBuilderProps {
     fields: InputFieldProps[]
 }
 
-const API_ENDPOINT =
-    'https://docs.google.com/forms/u/0/d/e/1FAIpQLSef0lyI1bMqQRlX6Yh2fobh9-4pz8WtLs7CjssIC9x6QGpqNA/formResponse'
+// const API_ENDPOINT =
+//     'https://docs.google.com/forms/u/0/d/e/1FAIpQLSef0lyI1bMqQRlX6Yh2fobh9-4pz8WtLs7CjssIC9x6QGpqNA/formResponse'
 
 const FormBuilder = ({ fields }: FormBuilderProps) => {
     const [status, setStatus] = useState<Status>('initial')
-    const [success, setSuccess] = useState(false)
-    const [faild, setFaild] = useState(false)
-
     const defaultValues = fields.map((f) => ({ [f.name]: '', confirm: false }))
 
     const schema = yup
         .object({
-            // company: yup.string().required(),
-            // name: yup.string().required(),
-            // kana: yup.string().required(),
-            // tel: yup.string().required(),
-            // email: yup.string().required().email(),
-            // // category: yup.string(),
-            // // .required()
-            // // .oneOf(['A', 'B'], 'お問い合わせ項目を選択してください'),
-            // message: yup.string(),
             confirm: yup
                 .bool()
                 .default(false)
@@ -77,46 +46,38 @@ const FormBuilder = ({ fields }: FormBuilderProps) => {
         handleSubmit,
         formState: { errors },
     } = useForm({
-        // defaultValues: schema.cast(),
         defaultValues,
-        // resolver: yupResolver(schema, {
-        //     stripUnknown: true,
-        //     abortEarly: false,
-        // }),
         criteriaMode: 'all',
         mode: 'all',
     })
 
-    const onSubmit = async (data: any) => {
+    const onSubmit = useCallback(async (data: any) => {
         setStatus('loading')
         const formData = new FormData()
 
-        Object.keys(data).map((key) => {
+        Object.keys(data).forEach((key) => {
             formData.append(key, data[key])
         })
 
         try {
-            await fetch(API_ENDPOINT, {
-                method: 'POST',
-                body: formData,
-            })
-                .then(async (response) => {
-                    const json: any = await response.json()
-                    console.log(json)
-                    json.code === 200 ? setStatus('sent') : setStatus('failure')
-                })
-                .catch(() => {
-                    setStatus('failure')
-                })
+            console.log('formData', formData)
+            setStatus('sent')
+            // await fetch(API_ENDPOINT, {
+            //     method: 'POST',
+            //     body: formData,
+            // })
+            //     .then(async (response) => {
+            //         const json: any = await response.json()
+            //         console.log(json)
+            //         json.code === 200 ? setStatus('sent') : setStatus('failure')
+            //     })
+            //     .catch(() => {
+            //         setStatus('failure')
+            //     })
         } catch (error) {
             setStatus('failure')
         }
-    }
-
-    useEffect(() => {
-        status === 'sent' ? setSuccess(true) : setSuccess(false)
-        status === 'failure' ? setFaild(true) : setFaild(false)
-    }, [status])
+    }, [])
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="inner">
@@ -146,12 +107,10 @@ const FormBuilder = ({ fields }: FormBuilderProps) => {
                     {['loading'].includes(status) && (
                         <span className="loading loading-spinner" />
                     )}
-                    {clsx({
-                        送信: status === 'initial',
-                        送信中です: status === 'loading',
-                        送信完了しました: status === 'sent',
-                        送信が失敗しました: status === 'failure',
-                    })}
+                    {status === 'initial' && '送信'}
+                    {status === 'loading' && '送信中です'}
+                    {status === 'sent' && '送信完了しました'}
+                    {status === 'failure' && '送信が失敗しました'}
                 </button>
             </div>
         </form>
